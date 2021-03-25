961370ddd2d9b08005ad2c03970722506408c36e
warita65 <wariyta9@gmail.com>
2021-03-25 17:52:42 -0400

    routing and functions

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
-
-
 app_name = 'apps'
 
 urlpatterns = [
     path('',views.index, name="index"),
+    path('contact',views.contact, name="contact"),
+    path('about',views.about, name="about"),
+    path('projects',views.projects, name="projects"),
+    path('services',views.services, name="services"),
+
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "app/index.html")
+
+def about(request):
+    return render(request, "app/index.html")
+
+def contact(request):
+    return render(request, "app/index.html")
+
+def projects(request):
+    return render(request, "app/index.html")
+
+def services(request):
+    return render(request, "app/index.html")
+
