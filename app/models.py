e20ed6a6b43142ecd79f10be85a79fc2a52a4064
warita65 <wariyta9@gmail.com>
2021-04-06 11:02:36 -0400

    navbar on work

--- app/models.py
+++ app/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class FeedbackContacts(models.Model):
-    full_name = models.CharField(max_length=200, default=""),
-    email = models.EmailField(max_length=200),
-    phone_number = models.CharField(max_length=10),
-    subject = models.CharField(max_length=200),
-    message = models.CharField(max_length=500),
+    full_name = models.CharField(max_length=200, default="")
+    email = models.EmailField(max_length=200, default="")
+    phone_number = models.CharField(max_length=10, default="")
+    subject = models.CharField(max_length=200, default="")
+    message = models.CharField(max_length=500, default="")
     date = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
