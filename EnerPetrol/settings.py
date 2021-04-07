f5dc574c00daf4a08cc7a57e70f85177059886b5
warita65 <wariyta9@gmail.com>
2021-04-07 18:44:55 -0400

    Heroku DB

--- EnerPetrol/settings.py
+++ EnerPetrol/settings.py
@@ -80,10 +80,19 @@ WSGI_APPLICATION = 'EnerPetrol.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# 'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
     }
 }
 
